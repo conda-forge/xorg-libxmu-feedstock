71ab68c8e53e1cb4ad869b521da3c1b7146cb13c
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2024-10-04 00:12:29 +0900

    Minor edit

--- recipe/build.sh
+++ recipe/build.sh
@@ -34,7 +34,6 @@ if [ -n "$CYGWIN_PREFIX" ] ; then
     export AUTOMAKE=automake-$am_version
     autoreconf_args=(
         --force
-        --verbose
         --install
         -I "$mprefix/share/aclocal"
         -I "$BUILD_PREFIX_M/Library/usr/share/aclocal"
@@ -55,7 +54,6 @@ else
 
     autoreconf_args=(
         --force
-        --verbose
         --install
         -I "${PREFIX}/share/aclocal"
         -I "${BUILD_PREFIX}/share/aclocal"
